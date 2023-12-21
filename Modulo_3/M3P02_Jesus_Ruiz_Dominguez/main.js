db66dcfaa020db142faed54d69e5a31c325f877b
jesruidom <ruiz.dominguez.jesus@gmail.com>
2023-12-21 15:03:23 +0100

    practicas 1 y 2 js hechas

---
+++ Modulo_3/M3P02_Jesus_Ruiz_Dominguez/main.js
@@ -0,0 +1,9 @@
+//Se pide el valor con Pront y se parsea a float para poder operar con él
+const radioEsfera = parseFloat(prompt(`Índica el radio de la esfera que se quiere calcular`));
+const formula = `4/3 * PI * radioEsfera^3`;
+const volumenEsfera = 4/3 * Math.PI * Math.pow(radioEsfera,3);
+
+document.write(`El volumen de la esfera se calcula con la fórmula ${formula}, por tanto una esfera de radio ${radioEsfera} tiene un volumen de ${volumenEsfera}`);
+
+
+
